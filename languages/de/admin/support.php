7cf3a98c5752a36d8dc5290e4eb7e4a290d541b6
Sebastian Schulz <sebastian.schulz@plath.de>
2018-10-30 16:15:05 +0100

    Language files

---
+++ languages/de/admin/support.php
@@ -0,0 +1,21 @@
+<?php
+
+$language_array = Array(
+    'historie'=>'Historie',
+    'no_new_ticket'=>'kein neues Ticket',
+    'no_ticket'=>'kein Support Ticket vorhanden',
+    'select_ticket_category'=>'W&auml;hle eine Kategorie aus, um das Template zu laden.',
+    'ticket_add'=>'Ticket anlegen',
+	'ticket_error_1'=>'Bitte gebe einen Betreff für das Ticket an.',
+	'ticket_error_2'=>'Bitte schreibe einen Text zu deinem Anliegen.',
+	'ticket_error_3'=>'Kein Benutzer ausgew&auml;hlt - Ticket konnte nicht erstellt werden.',
+    'ticket_no_cup'=>'kein Cup vorhanden',
+    'ticket_status_1'=>'offen',
+    'ticket_status_2'=>'in Bearbeitung',
+    'ticket_status_3'=>'geschlossen',
+    'ticket_take'=>'Ticket nehmen',
+    'unknown_admin'=>'unbekannter Admin',
+    'unknown_support_data'=>'unbekannte Support-Daten',
+    'unknown_ticket'=>'unbekanntes Support-Ticket',
+    'view'=>'ansehen'
+);
