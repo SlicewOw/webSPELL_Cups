ddc5962febb8f3263f9a69ae1b8c890ebf32a589
Sebastian Schulz <sebastian.schulz@plath.de>
2018-11-07 09:13:53 +0100

    Add missing admin language files

---
+++ languages/en/admin/support.php
@@ -0,0 +1,21 @@
+<?php
+
+$language_array = Array(
+    'historie'=>'history',
+    'no_new_ticket'=>'no new ticket',
+    'no_ticket'=>'no ticket',
+    'select_ticket_category'=>'select ticket category',
+    'ticket_add'=>'add ticket',
+    'ticket_error_1'=>'no subject',
+    'ticket_error_2'=>'no text',
+    'ticket_error_3'=>'no user',
+    'ticket_no_cup'=>'no cup',
+    'ticket_status_1'=>'open',
+    'ticket_status_2'=>'in progress',
+    'ticket_status_3'=>'closed',
+    'ticket_take'=>'take ticket',
+    'unknown_admin'=>'unknown admin',
+    'unknown_support_data'=>'unknown support data',
+    'unknown_ticket'=>'unknown ticket',
+    'view'=>'view'
+);
---
+++ languages/en/admin/teams.php
@@ -0,0 +1,25 @@
+<?php
+
+$language_array = Array(
+    'aktiv'=>'actual logo',
+    'add'=>'add team',
+    'error_logotype'=>'Error while saving logotype',
+    'existing_teams'=>'Existing teams',
+    'general_stats'=>'general statistics',
+    'info'=>'Info',
+    'most_played_matches'=>'Most played matches',
+    'new'=>'add team',
+    'newest_clan'=>'newest clan',
+    'no_match'=>'no match',
+    'no_search_parameter'=>'no search parameter!',
+    'no_team'=>'no team',
+    'teams'=>'Teams',
+    'team_name'=>'Teamname',
+    'search'=>'Search..',
+    'sname'=>'Team Tag',
+    'sort'=>'Sorting',
+    'total_deleted'=>'Delete totally',
+    'turl'=>'URL',
+    'purl'=>'Teampic',
+    'url'=>'URLs'
+);
