277402e6a5aa84c8b93bdf692e88d99ae702b100
SlicewOw <slicewow@myrisk-gaming.de>
2019-03-12 08:38:55 +0100

    SlicewOw/webSPELL_Cups#120 - Fix cronjob and image path

--- cup/cj/twitch.php
+++ cup/cj/twitch.php
@@ -19,24 +19,20 @@ try {
 
     $idArray = array();
 
-    $info = mysqli_query(
-        $_database,
+    $info = cup_query(
         "SELECT
               `id`
             FROM `" . PREFIX . "liveshow`
-            WHERE cronjobID = " . $cronjob_id . " AND type = 1"
+            WHERE cronjobID = " . $cronjob_id . " AND type = 1",
+        __FILE__
     );
 
-    if (!$info) {
-        throw new \Exception('info_query_failed');
-    }
-
-    while( $db = mysqli_fetch_array( $info ) ) {
+    while ($db = mysqli_fetch_array($info)) {
         $idArray[] = strtolower($db['id']);
     }
 
     $anzStreams = count($idArray);
-    if($anzStreams < 1) {
+    if ($anzStreams < 1) {
         throw new \Exception('no_streams_available_cronjob_' . $cronjob_id);
     }
 
@@ -52,13 +48,17 @@ try {
     // Open Data URL
     $result = getAPIData($json_url, 'twitch');
 
+    if (validate_array($result)) {
+        throw new \Exception('unknown_attribute_result');
+    }
+
     //
     // Data Array
     $json_array = json_decode($result, true);
 
     if (isset($_GET['debug']) && validate_int($_GET['debug'])) {
 
-        if($_GET['debug'] >= 1) {
+        if ($_GET['debug'] >= 1) {
 
             $returnArray['twitch_id'] = $idArray;
 
@@ -82,7 +82,7 @@ try {
         'offline'   => 0
     );
 
-    $anz = (isset($json_array['_total'])) ? 
+    $anz = (isset($json_array['_total'])) ?
         $json_array['_total'] : 0;
 
     for ($x = 0; $x < $anz; $x++) {
@@ -100,32 +100,28 @@ try {
         $pic = isset($json_array['streams'][$x]['preview']['medium']) ?
             $json_array['streams'][$x]['preview']['medium'] : '';
 
-        if (!empty($pic) && @copy($pic, __DIR__ . '/../../images/media/streams/' . $twitch_id . '.jpg')) {
+        if (!empty($pic) && @copy($pic, __DIR__ . '/../../images/cup/streams/' . $twitch_id . '.jpg')) {
             $setValueArray[] = 'preview = \'' . addslashes($twitch_id) . '.jpg\'';
         }
 
         //
         // save stream details
-        if(isset($json_array['streams'][$x]['viewers'])) {
+        if (isset($json_array['streams'][$x]['viewers'])) {
             $setValueArray[] = '`viewer` = ' . (int)$json_array['streams'][$x]['viewers'];
         }
 
-        if(isset($json_array['streams'][$x]['viewers'])) {
+        if (isset($json_array['streams'][$x]['viewers'])) {
             $setValueArray[] = '`game` = \'' . addslashes($json_array['streams'][$x]['game']) . '\'';
         }
 
         $setValue = implode(', ', $setValueArray);
 
-        $updateQuery = mysqli_query(
-            $_database,
-            "UPDATE `" . PREFIX . "liveshow` 
+        $updateQuery = cup_query(
+            "UPDATE `" . PREFIX . "liveshow`
                 SET " . $setValue . "
-                WHERE `id` = '" . $twitch_id . "'"
+                WHERE `id` = '" . $twitch_id . "'",
+            __FILE__
         );
-
-        if (!$updateQuery) {
-            throw new \Exception('update_stream_query_failed');
-        }
 
         $anzStreamsArray['online']++;
 
@@ -139,22 +135,19 @@ try {
     //
     // set all offline streams
     $anzStreamsOffline = count($idArray);
-    if($anzStreamsOffline > 0) {
+    if ($anzStreamsOffline > 0) {
 
         $anzStreamsArray['offline'] = $anzStreamsOffline;
 
         $implodeArray = '\'' . implode('\',\'', $idArray) . '\'';
-        $updateQuery = mysqli_query(
-            $_database,
-            "UPDATE `" . PREFIX . "liveshow` 
-                SET `online` = 0, 
-                    `viewer` = 0 
-                WHERE `id` IN (" . $implodeArray . ")"
-        );
 
-        if (!$updateQuery) {
-            throw new \Exception('update_query_failed (' . $implodeArray . ')');
-        }
+        $updateQuery = cup_query(
+            "UPDATE `" . PREFIX . "liveshow`
+                SET `online` = 0,
+                    `viewer` = 0
+                WHERE `id` IN (" . $implodeArray . ")",
+            __FILE__
+        );
 
         for ($x = 0; $x < $anzStreamsOffline; $x++) {
 
@@ -167,22 +160,18 @@ try {
                 }
 
                 $json_url = 'https://api.twitch.tv/kraken/channels/' . $twitch_id;
-                if($json_data = getAPIData($json_url, 'twitch')) {
+                if ($json_data = getAPIData($json_url, 'twitch')) {
 
                     $twitchData = json_decode($json_data, TRUE);
                     if (!isset($twitchData['status']) || ($twitchData['status'] != '404')) {
                         throw new \Exception('stream_is_existing_do_not_delete');
                     }
 
-                    $deleteQuery = mysqli_query(
-                        $_database,
+                    $deleteQuery = cup_query(
                         "DELETE FROM `". PREFIX . "liveshow`
-                            WHERE id = '" . $twitch_id . "'"
+                            WHERE `id` = '" . $twitch_id . "'",
+                        __FILE__
                     );
-
-                    if (!$deleteQuery) {
-                        throw new \Exception('delete_stream_query_failed (' . $twitch_id . ')');
-                    }
 
                 }
 
@@ -192,35 +181,30 @@ try {
 
     }
 
-    $checkIf = mysqli_fetch_array(
-        mysqli_query(
-            $_database,
-            "SELECT 
-                    COUNT(*) AS `exist` 
-                FROM `" . PREFIX . "liveshow_cronjobs` 
-                WHERE `id` = " . $cronjob_id
-        )
+    $selectQurey = cup_query(
+        "SELECT
+                COUNT(*) AS `exist`
+            FROM `" . PREFIX . "liveshow_cronjobs`
+            WHERE `id` = " . $cronjob_id,
+        __FILE__
     );
+
+    $checkIf = mysqli_fetch_array($selectQurey);
 
     if ($checkIf['exist'] == 1) {
 
-        $query = mysqli_query(
-            $_database,
-            "UPDATE `" . PREFIX . "liveshow_cronjobs` 
-                SET hits = hits + 1, 
-                    date = " . time() . " 
-                WHERE id = " . $cronjob_id
+        $query = cup_query(
+            "UPDATE `" . PREFIX . "liveshow_cronjobs`
+                SET `hits` = `hits` + 1,
+                    `date` = " . time() . "
+                WHERE `id` = " . $cronjob_id,
+        __FILE__
         );
-
-        if (!$query) {
-            throw new \Exception('update_cj_query_failed');
-        }
 
     } else {
 
-        $query = mysqli_query(
-            $_database,
-            "INSERT INTO `" . PREFIX . "liveshow_cronjobs` 
+        $query = cup_query(
+            "INSERT INTO `" . PREFIX . "liveshow_cronjobs`
                 (
                     `id`,
                     `hits`,
@@ -231,12 +215,9 @@ try {
                     " . $cronjob_id . ",
                     1,
                     " . time() . "
-                )"
+                )",
+            __FILE__
         );
-
-        if (!$query) {
-            throw new \Exception('insert_cj_query_failed');
-        }
 
     }
 
