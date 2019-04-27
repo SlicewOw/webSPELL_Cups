ded07a17df0e60f4a8475a4bc08c132cbb6b9422
SlicewOw <slicewow@myrisk-gaming.de>
2019-04-27 12:36:27 +0200

    Hotfix to upload JPEG files as cup banner or icon

--- admin/cup_admin/includes/cup_details_images.php
+++ admin/cup_admin/includes/cup_details_images.php
@@ -5,26 +5,26 @@ try {
     $filepath = '../images/cup/banner/';
 
     if (validate_array($_POST, true)) {
-           
+
         $_language->readModule('formvalidation', true);
-        
+
         $parent_url = 'admincenter.php?site=cup&mod=cup&action=cup&id=' . $cup_id . '&page=images';
-        
+
         try {
-            
+
             if (isset($_POST['submitCupImages'])) {
-                
+
                 $allowedCupImagesArray = array(
                     'cup_icon',
                     'cup_banner'
                 );
-                
+
                 $cupSetImagesArray = array();
 
                 $errors = array();
 
                 foreach ($allowedCupImagesArray as $image_key) {
-                    
+
                     if (isset($_FILES[$image_key])) {
 
                         $upload = new \webspell\HttpUpload($image_key);
@@ -38,6 +38,7 @@ try {
 
                                 $mime_types = array(
                                     'image/jpg',
+                                    'image/jpeg',
                                     'image/gif',
                                     'image/png'
                                 );
@@ -67,31 +68,31 @@ try {
                             }
 
                         }
-                        
+
                     }
-                    
+
                 }
 
                 if (validate_array($errors, true)) {
                     $_SESSION['errorArray'] = $errors;
                 }
-                
+
                 if (validate_array($cupSetImagesArray, true)) {
-                    
+
                     $updateQuery = cup_query(
                         "UPDATE `" . PREFIX . "cups`
                             SET " . implode(', ', $cupSetImagesArray) . "
                             WHERE `cupID` = " . $cup_id,
                         __FILE__
                     );
-                    
+
                 }
-                
+
             } else if (isset($_POST['deleteCupImage_icon']) || isset($_POST['deleteCupImage_banner'])) {
-                
+
                 $image_key = (isset($_POST['deleteCupImage_icon'])) ?
                     'cup_icon' : 'cup_banner';
-                
+
                 $image = getCupImage($cup_id, $image_key, false);
 
                 $imagePath = __DIR__ . '/../../../images/cup/banner/' . $image;
@@ -104,49 +105,49 @@ try {
                     }
 
                 }
-                
+
                 $updateQuery = cup_query(
                     "UPDATE `" . PREFIX . "cups`
                         SET `" . $image_key . "` = NULL
                         WHERE `cupID` = " . $cup_id,
                     __FILE__
                 );
-                
+
             } else {
                 throw new \Exception($_languages->module['unknown_action']);
             }
-            
+
         } catch (Exception $e) {
             $_SESSION['errorArray'][] = $e->getMessage();
         }
-        
+
         header('Location: ' . $parent_url);
-        
+
     } else {
 
         if (!isset($cupArray) || !validate_array($cupArray, true)) {
             $cupArray = getcup($cup_id);
         }
-        
+
         $cupImageArray = array(
             'icon',
             'banner'
         );
-        
+
         $imageArray = array();
 
         foreach ($cupImageArray as $image_key) {
-        
+
             $image_value = (!empty($cupArray['images'][$image_key])) ?
                 '<img src="' . $cupArray['images'][$image_key] . '" alt="" class="img-responsive" />' : '';
-        
+
             if (!empty($image_value)) {
-                
+
                 $deleteButtonAttributeArray = array();
                 $deleteButtonAttributeArray[] = 'type="submit"';
                 $deleteButtonAttributeArray[] = 'name="deleteCupImage_' . $image_key . '"';
                 $deleteButtonAttributeArray[] = 'class="btn btn-danger btn-xs white darkshadow"';
-                
+
                 $image_value .= '<br /><button ' . implode(' ', $deleteButtonAttributeArray) . '>' . $_language->module['delete'] . '</button>';
             }
 
