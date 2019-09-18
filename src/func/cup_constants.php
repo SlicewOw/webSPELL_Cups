c011d966e88495dd4e6a0748cca5d8cb9374ad30
Sebastian Schulz <sebastian.schulz@plath.de>
2019-09-18 09:05:00 +0200

    Add missing func file

---
+++ src/func/cup_constants.php
@@ -0,0 +1,57 @@
+<?php
+
+function getConstNameStatus() {
+    return 'status';
+}
+
+function getConstNameMessage() {
+    return 'message';
+}
+
+function getConstNameError() {
+    return 'error';
+}
+
+function getConstNameDebug() {
+    return 'debug';
+}
+
+function getConstNameCupId() {
+    return 'cupID';
+}
+
+function getConstNameCupIdWithUnderscore() {
+    return 'cup_id';
+}
+
+function getConstNamePhase() {
+    return 'phase';
+}
+
+function getConstNameStart() {
+    return 'start';
+}
+
+function getConstNameCheckIn() {
+    return 'checkin';
+}
+
+function getConstNameTeamId() {
+    return 'teamID';
+}
+
+function getConstNameTeamIdWithUnderscore() {
+    return 'team_id';
+}
+
+function getConstNameCronjobId() {
+    return 'cj_id';
+}
+
+function getConstNameStreams() {
+    return 'streams';
+}
+
+function getConstNameViewers() {
+    return 'viewers';
+}
